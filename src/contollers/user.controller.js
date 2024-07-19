d7763fd4390203e0d17a4a5d75eb47216ebfb0ff
reyanshravi <ravi8298922623@gmail.com>
2024-07-19 23:57:15 +0530

    User Controllers updated.

--- src/contollers/user.controller.js
+++ src/contollers/user.controller.js
@@ -1,9 +1,13 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
-  });
+  const { fullName, email, username, password } = req.body;
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+  }
 });
 
 export default registerUser;
